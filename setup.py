27ac49b055d2420de5a7b53d597db6a81627730d
Xinyue Pan <rena9155@gmail.com>
2023-06-29 13:45:31 +0200

    first commit - standard data science project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict whether a customer will churn or not for Telcom',
+    author='Rena',
+    license='',
+)
